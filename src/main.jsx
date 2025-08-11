cd4832009bc8acc6ba009e0de27f93a04da24586
Fabel Biju <158803896+Abrahamozler@users.noreply.github.com>
2025-08-12 02:23:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom'; // Add this import back
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>  {/* Add this component back */}
+      <App />
+    </BrowserRouter> {/* Add this component back */}
   </React.StrictMode>,
 );
