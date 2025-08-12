b8bbb89d48bca78a50385682624b80b1c48446d6
Fabel Biju <158803896+Abrahamozler@users.noreply.github.com>
2025-08-12 19:12:30 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -4,10 +4,15 @@ const api = axios.create({
   baseURL: 'https://fixtrack-backend.onrender.com/api',
 });
 
+// This interceptor is crucial. It adds the user's login token to every API request.
 api.interceptors.request.use((config) => {
-  const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-  if (userInfo && userInfo.token) {
-    config.headers.Authorization = `Bearer ${userInfo.token}`;
+  try {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+    if (userInfo && userInfo.token) {
+      config.headers.Authorization = `Bearer ${userInfo.token}`;
+    }
+  } catch (error) {
+    console.error("Could not parse user info from localStorage", error);
   }
   return config;
 });
